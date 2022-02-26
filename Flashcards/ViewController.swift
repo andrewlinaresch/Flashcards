f02065fded6fa2052ebd0bf70dfc2359e0089357
Andrew Linares <andrewlinaresch@gmail.com>
2022-02-26 14:50:35 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Andrew Linares on 2/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        questionLabel.isHidden = true
+    }
+}
+
